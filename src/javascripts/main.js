bfe0f810501f60c0b11534cbd052e8694f679b9a
Lwam Tekie <ltekie87@yahoo.com>
2019-05-30 20:07:45 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,20 @@
-import '../styles/main.scss';
-import 'bootstrap';
+import firebase from 'firebase/app';
 
-console.error('hi');
+import Auth from './components/auth/auth';
+
+import MyNavbar from './components/MyNavbar/MyNavbar';
+
+
+import apiKeys from './helpers/apiKeys.json';
+
+import '../styles/main.scss';
+
+//  initialize app makes an asynchronous request to firebase with you credential
+const init = () => {
+  firebase.initializeApp(apiKeys.firebaseKeys);
+  console.error(apiKeys.firebaseKeys);
+  MyNavbar.navbarEvents();
+  Auth.authStringBuilder();
+};
+
+init();
